e5cfa62dbbf501de46ebba9b2d84f9db7a6701b8
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-12-09 16:23:17 +0000

    Added socket.io setup at server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const socket = require('socket.io');
 
 // Start Server
-app.listen(process.env.PORT || 3000, function() {
+const server = app.listen(process.env.PORT || 3000, function() {
   console.log(`Listening on port ${process.env.PORT}`);
 });
+
+// Socket.io Setup
+const io = socket(server);
