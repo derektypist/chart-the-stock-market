a5cfd8ced4b4c4ec5ed5658b6efe00fc2865740e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-12-09 16:49:03 +0000

    Added route in server.js

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const socket = require('socket.io');
 
+// Index Page (Static HTML)
+app.route('/')
+  .get(function (req,res) {
+    res.sendFile(process.cwd + '/views/index.html');
+  });
+
+
 // Start Server
 app.listen(process.env.PORT || 3000, function() {
   console.log(`Listening on port ${process.env.PORT}`);
