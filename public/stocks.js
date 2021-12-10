cd188654cd18995cca0ab23cca21695bd66cafd0
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-12-10 10:17:22 +0000

    Added socket connection and Query DOM at stocks.js

--- public/stocks.js
+++ public/stocks.js
@@ -1,1 +1,11 @@
 let stockData = {};
+
+// Socket Connection
+const socket = io.connect('https://chart-the-stock-market.ddxps46.repl.co/');
+
+// Query DOM
+const symbolInput = document.getElementById('symbol-input');
+const getStockBtn = document.getElementById('get-stock-btn');
+const stockCanvas = document.getElementById('stock-canvas');
+const allElements = document.getElementsByTagName('*');
+let loading = true;
